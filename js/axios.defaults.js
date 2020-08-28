6c57f14a8ba514e422ba8360948a6a6f6c77cdc1
qiule123456 <2287318236@qq.com>
2020-08-28 19:59:30 +0800

    登录完成

---
+++ js/axios.defaults.js
@@ -0,0 +1,45 @@
+axios.defaults.baseURL = "http://localhost:8888";//配置基本请求路径
+//数据以表单的形式扔给服务器
+
+axios.defaults.headers['Content-Type'] = 'application/x-www-form-urlencoded';
+axios.defaults.transformRequest = function (data) {
+    if (!data) return data;
+    let result = ``;
+    for (let attr in data) {
+        if (!data.hasOwnProperty(attr)) break;
+        result += `&${attr}=${data[attr]}`;
+    }
+    return result.substring(1);
+};
+//配置请求拦截器
+axios.interceptors.request.use(config =>{
+    return config
+})
+//配置响应拦截器
+axios.interceptors.response.use(response =>{
+    return response.data;
+},reason=>{
+    // console.log("----------------------")
+    // console.log(reason)
+    // console.log("----------------------")
+    if (reason.response){
+        switch (String(reason.response.status)){
+            case"404":
+            alert("请求地址不存在")
+            break;
+            default:
+                break;
+        }
+    }
+    return Promise.reject(reason);
+}
+
+)
+
+
+
+
+
+
+
+
